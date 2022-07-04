826a5decdec1e95056e1c15b28dcd4acbb143ebd
haesa <sky615600@gmail.com>
2022-07-04 17:42:43 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -1,48 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import './app.css';
-import Header from './components/header/header';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
-import popularList from './popular.json';
-import searchList from './search.json';
+// import popularList from './popular.json';
+// import searchList from './search.json';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-
-  const handleOnSubmit = (event, value) => {
+  const search = (event, query) => {
     event.preventDefault();
-    console.log(value);
-    // const requestOptions = {
-    //   method: 'GET',
-    //   redirect: 'follow',
-    // };
-    // fetch(
-    //   `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResult=25&q=${value}&key=${API_KEY}`,
-    //   requestOptions
-    // )
-    //   .then((response) => response.json())
-    //   .then((result) => setVideos(result.items))
-    //   .catch((error) => console.log('error:', error));
-    setVideos(searchList.items);
+    console.log(query);
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
   };
 
   useEffect(() => {
-    // const requestOptions = {
-    //   method: 'GET',
-    //   redirect: 'follow',
-    // };
-    // fetch(
-    //   'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=${API_KEY}',
-    //   requestOptions
-    // )
-    //   .then((response) => response.json())
-    //   .then((result) => setVideos(result.items))
-    //   .catch((error) => console.log('error:', error));
-    setVideos(popularList.items);
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
   return (
-    <div className="main">
-      <Header onSubmit={handleOnSubmit} />
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,36 @@
+import React, { useRef } from 'react';
+import styles from './search_header.module.css';
+
+const SearchHeader = ({ onSearch }) => {
+  const inputRef = useRef();
+  const handleSearch = (event) => {
+    onSearch(event, inputRef.current.value);
+    inputRef.current.value = '';
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img className={styles.img} src="../../../images/logo.png" alt="logo" />
+        <h1 className={styles.title}>Youtube</h1>
+      </div>
+      <form className={styles.search} onSubmit={handleSearch}>
+        <input
+          ref={inputRef}
+          className={styles.input}
+          type="text"
+          placeholder="Search..."
+        />
+        <button className={styles.button}>
+          <img
+            className={styles.buttonImg}
+            src="../../../images/search.png"
+            alt="search"
+          />
+        </button>
+      </form>
+    </header>
+  );
+};
+
+export default SearchHeader;
